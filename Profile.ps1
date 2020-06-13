b6f4822bfe9437a266e1f6d48baaefc59d0cf391
0xW1sKy <20827857+0xW1sKy@users.noreply.github.com>
2020-06-12 19:17:40 -0500

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -4,6 +4,6 @@ function global:prompt {
   Write-Host($env:computername) -nonewline -foregroundcolor Green
   Write-Host("] ") -nonewline
   Write-Host("$(Split-Path $pwd -Leaf)/") -nonewline -foregroundcolor Blue
-  return " $ "
+  return " $`: "
 }
 Invoke-RestMethod -uri https://raw.githubusercontent.com/0xW1sKy/PowerShellTools/master/rebellogo | % { $_.split("`n") | % { Write-Host $_ -ForegroundColor red -BackgroundColor black; Start-Sleep .05 }}
